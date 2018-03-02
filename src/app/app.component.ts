e4d32cbe42ab6f82c56d3f6c794e7e3427530f2d
Wessel Loth <wessel@loth.io>
2018-03-02 11:58:57 +0100

    Implemented ERP system

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  time = new Date();
+  get answer() {
+    return this.time.getHours() >= 12;
+  }
+
+  constructor() {
+    Observable.interval(1000).subscribe(() => {
+      this.time = new Date();
+    });
+  }
+}
